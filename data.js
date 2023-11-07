ce04f303a7202fe8f34d8ee0de3de67af634ac03
arthur1rt <shalkioth@gmail.com>
2023-11-06 19:13:30 -0800

    EP instead of mixtape

--- data.js
+++ data.js
@@ -14,7 +14,7 @@ musicData = [
         "apple": "https://music.apple.com/us/album/postura-de-empres%C3%A1ria-single/1677652308"
     },
     {
-        "title": "[Mixtape] Eu Num Quarto",
+        "title": "[EP] Eu Num Quarto",
         "image": "images/Eu num quarto.png",
         "spotify": "https://open.spotify.com/album/5YYqJl74SveMAIDW2XGQ9z?si=1I3ChnKgQUybC_k1eZtqYA",
         "youtube": "https://www.youtube.com/watch?v=D1003-XQJU0&list=PLoLpLgc9S_WNRacQueN6rSm7V9yiRoxv8&ab_channel=yKouhai",
