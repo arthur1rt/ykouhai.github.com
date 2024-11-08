d95dd8948219e53af6ad6f2434311f94018729cf
arthur1rt <shalkioth@gmail.com>
2024-11-07 18:57:16 -0800

    updating links

--- data.js
+++ data.js
@@ -49,23 +49,24 @@ musicData = [
     //     "apple": "https://music.apple.com/us/album/postura-de-empres%C3%A1ria-single/1677652308",
     //     "deezer": "https://deezer.page.link/5iJyBtcrb7DZkRSA6",
     //     "tidal": "https://tidal.com/browse/album/282288980",
+    // },
+    // {
+    //     "page_name": "EuNumQuarto",
+    //     "title": "[EP] Eu Num Quarto",
+    //     "image": "images/Eu num quarto.png",
+    //     "spotify": "https://open.spotify.com/album/5YYqJl74SveMAIDW2XGQ9z?si=1I3ChnKgQUybC_k1eZtqYA",
+    //     "youtube": "https://www.youtube.com/watch?v=D1003-XQJU0&list=PLoLpLgc9S_WNRacQueN6rSm7V9yiRoxv8&ab_channel=yKouhai",
+    //     "apple": "https://music.apple.com/us/album/eu-num-quarto/1654429592",
+    //     "amazon": "https://music.amazon.com/albums/B0CW269G9T?marketplaceId=ART4WZ8MWBX2Y&musicTerritory=CA&ref=dm_sh_tr0XVAKz521slfrUklEYoSDWT",
+    //     "deezer": "https://deezer.page.link/F1RKxjYZ5aiXzzVA8",
+    //     "tidal": "https://tidal.com/browse/album/297412033",
     // }
 ]
 
 
-/*
-"page_name": "EuNumQuarto",
-"title": "[EP] Eu Num Quarto",
-"image": "images/Eu num quarto.png",
-"spotify": "https://open.spotify.com/album/5YYqJl74SveMAIDW2XGQ9z?si=1I3ChnKgQUybC_k1eZtqYA",
-"youtube": "https://www.youtube.com/watch?v=D1003-XQJU0&list=PLoLpLgc9S_WNRacQueN6rSm7V9yiRoxv8&ab_channel=yKouhai",
-"apple": "https://music.apple.com/us/album/eu-num-quarto/1654429592",
-"amazon": "https://music.amazon.com/albums/B0CW269G9T?marketplaceId=ART4WZ8MWBX2Y&musicTerritory=CA&ref=dm_sh_tr0XVAKz521slfrUklEYoSDWT",
-"deezer": "https://deezer.page.link/F1RKxjYZ5aiXzzVA8",
-"tidal": "https://tidal.com/browse/album/297412033",
 
 
 
 
 
-*/
+
